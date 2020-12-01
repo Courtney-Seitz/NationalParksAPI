efd6440a1634975bd6889eee9db211aef786a07a
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2020-12-01 15:31:01 -0500

    fixed multiple state search issue

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 'use strict';
 
-// put your own value below!
 const apiKey = 'rxVneIU21uinLUrDPvfBZfILM1MUjOhwzLlI1cCo'; 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
 
-function formatQueryParams(params) {
-const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-return queryItems.join('&');
-}
-
 function displayResults(responseJson, maxResults) {
-  // if there are previous results, remove them
-console.log(responseJson);
-$('#results-list').empty();
-  // iterate through the items array
-for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
+
+    $('#results-list').empty();
+
+for (let i = 0; i < responseJson.data.length; i++) {
     $('#results-list').append(`<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
-    <p>${responseJson.data[i].description}</p>
-    </li>`);
+    <p><strong>Description:</strong> ${responseJson.data[i].description}</p>
+    <p><strong>States:</strong> ${responseJson.data[i].states}</p></li>`);
 };
-  //display the results section  
+
 $('#results').removeClass('hidden');
 };
 
-function getParks(searchURL, stateArr, maxResults, apiKey) {
-
+function getParks(stateArr, maxResults = 10) {
     const params = {
         stateCode: stateArr,
+        api_key: apiKey,
         limit: maxResults
     }
 
-    // const queryString = formatQueryParams(params);
-    const url = searchURL + '?' + formatQueryParams(params) + '&api_key=' + apiKey;
+    const url = `${searchURL}?api_key=${apiKey}&stateCode=${stateArr}&limit=${maxResults}`;
     console.log(url);
 
 fetch(url)
@@ -50,13 +41,12 @@ fetch(url)
 }
 
 function watchForm() {
-$('#js-form').submit(event => {
+$('#js-form').on('submit', function(event) {
     event.preventDefault();
-    // const searchURL = 'https://developer.nps.gov/api/v1/parks';
-    const stateArr = $('#js-search-term').val().split(",");
+    const stateArr = $('#js-search-term').val();
+    console.log(stateArr);
     const maxResults = $('#js-max-results').val();
-    getParks(searchURL, stateArr, maxResults, apiKey);
-
+    getParks(stateArr, maxResults);
 });
 }
 
